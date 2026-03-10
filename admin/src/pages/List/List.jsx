00d6639ced6d51d92a1173a065e901f9dac7aeef
RithmaRandima <rithma.r.nandasiri@gmail.com>
2026-03-10 11:37:06 +0530

    Completed List Item Page

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return <div>List</div>;
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.status) {
+        setList(response.data.data);
+      } else {
+        toast.error("Failed to Load Data");
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log(error);
+    }
+  };
+
+  const removeFood = async (foodID) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodID });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Failed to Delete Food");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add ">
+      <p>All Foods List</p>
+      <hr />
+      {/* list container */}
+      <div className="list-table">
+        {/* list container format */}
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+
+        {list.map((item) => (
+          <div className="list-table-format" key={item._id}>
+            <img src={`${url}/images/${item.image}`} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{item.price}</p>
+            <p className="" onClick={() => removeFood(item._id)}>
+              x
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default List;
